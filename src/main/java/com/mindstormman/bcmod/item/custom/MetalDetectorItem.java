b9da82a2676764bcc8b74baa661e41dc36b2b46b
Mindstormman06 <aidenadzich@gmail.com>
2023-11-10 23:35:04 -0800

    Added an empty class for custom tags

--- src/main/java/com/mindstormman/bcmod/item/custom/MetalDetectorItem.java
+++ src/main/java/com/mindstormman/bcmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package com.mindstormman.bcmod.item.custom;
 
+import com.mindstormman.bcmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -10,6 +11,8 @@ import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.tags.ITagManager;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties pProperties) {
@@ -52,7 +55,7 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(Tags.Blocks.ORES);
 
     }
 }
---
+++ src/main/java/com/mindstormman/bcmod/util/ModTags.java
@@ -0,0 +1,28 @@
+package com.mindstormman.bcmod.util;
+
+import com.mindstormman.bcmod.BCMod;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+
+    public static class Blocks {
+
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(BCMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+
+
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(BCMod.MOD_ID, name));
+        }
+    }
+}
