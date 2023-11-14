bb10fd966adf7ec4f1f5cb80449aea1ca45a0132
Mindstormman06 <aidenadzich@gmail.com>
2023-11-13 16:09:27 -0800

    Webhook Test

--- src/main/java/com/mindstormman/bcmod/BCMod.java
+++ src/main/java/com/mindstormman/bcmod/BCMod.java
@@ -25,6 +25,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BCMod.MOD_ID)
 public class BCMod
+    //Webhook Test
 {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "britishcolumbiamod";
